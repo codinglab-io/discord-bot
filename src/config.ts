01f0e5acd2be4bfd4227b7e8b1687bbf4f34eb5e
Peïo Thibault <peio.thibault@gmail.com>
2023-05-18 01:45:35 +0200

    add cache

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,7 @@ export const config = {
   discord: {
     token: env.get('DISCORD_TOKEN').required().asString(),
   },
+  redis: {
+    url: env.get('REDIS_URL').required().asString(),
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { Client } from 'discord.js';
 
-import { bootstrap } from './bootstrap';
+import { config } from './config';
+
+const bootstrap = async (client: Client) => {
+  await client.login(config.discord.token);
+
+  await new Promise<void>((resolve) => {
+    client.on('ready', () => {
+      resolve();
+    });
+  });
+
+  if (!client.isReady()) {
+    throw new Error('Client should be ready at this stage');
+  }
+};
 
 const client = new Client({
   intents: ['Guilds', 'GuildVoiceStates', 'GuildMembers'],
