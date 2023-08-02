566064a6dce84496009884f28a63ec8d06d9978f
LazyRabbit <lmontaigut@gmail.com>
2023-08-02 21:45:49 +0200

    feat: add 'fart' slash command (#26)
    Co-authored-by: Peïo Thibault <peio.thibault@gmail.com>

--- src/commands.ts
+++ src/commands.ts
@@ -4,6 +4,11 @@ export const voiceOnDemandCommand = new SlashCommandBuilder()
   .setName('voice-on-demand')
   .setDescription('Actions related to the voice lobby')
   .addSubcommand((subcommand) =>
-    subcommand.setName('create').setDescription('Creates the voice lobby')
+    subcommand.setName('create').setDescription('Creates the voice lobby'),
   )
+  .toJSON();
+
+export const fartCommand = new SlashCommandBuilder()
+  .setName('fart')
+  .setDescription("Replies with https://prout.dev")
   .toJSON();
