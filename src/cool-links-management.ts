5917f5e0996e3c2719d3eb1b270d92cb757ccd8c
LazyRabbit <lmontaigut@gmail.com>
2023-08-02 21:51:06 +0200

    feat: cool links voting system and autothread (#27)

---
+++ src/cool-links-management.ts
@@ -0,0 +1,44 @@
+import { type Message, ThreadAutoArchiveDuration } from 'discord.js';
+import ogs from 'open-graph-scraper';
+
+const getThreadNameFromOpenGraph = async (url: string): Promise<string | null> => {
+  try {
+    const { result } = await ogs({ url });
+    if (!result.success) throw new Error('No OG data found');
+
+    const ogSiteName = result.ogSiteName;
+    const ogTitle = result.ogTitle;
+    if (ogSiteName && ogTitle) {
+      return `${ogSiteName} - ${ogTitle}`;
+    }
+    if (ogSiteName) {
+      return ogSiteName;
+    }
+    if (ogTitle) {
+      return ogTitle;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return null;
+};
+
+export const coolLinksManagement = async (message: Message) => {
+  const urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+  const detectedURLs = message.content.match(urlRegex);
+
+  if (detectedURLs === null) {
+    await message.delete();
+    return;
+  }
+
+  await message.react('✅');
+  await message.react('❌');
+
+  const threadName = await getThreadNameFromOpenGraph(detectedURLs[0]);
+  await message.startThread({
+    name: threadName ?? message.content,
+    autoArchiveDuration: ThreadAutoArchiveDuration.ThreeDays,
+  });
+};
