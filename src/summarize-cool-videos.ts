5600f409dfa9934f16a6cac3cccc79a262387e06
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-08-20 16:32:33 +0200

    refactor: rm playwright, just use fetch

--- src/summarize-cool-videos.ts
+++ src/summarize-cool-videos.ts
@@ -1,35 +1,43 @@
-import { chromium } from 'playwright';
-
-const summarizeUrl = 'https://www.summarize.tech/';
+const baseUrl = 'https://www.summarize.tech/api/summary';
 
 export const getVideoSummary = async (videoUrl: string) => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  await page.goto(summarizeUrl + videoUrl.replace('https://', '').replace('http://', ''));
-
-  const getSummary = async () => {
-    const startTime = await page.getByRole('link', { name: '00:00:00' }).elementHandle();
-    if (!startTime) return;
-
-    const summaryEl = await startTime.evaluateHandle(
-      (node) => node.parentElement?.nextElementSibling,
+  const summary = await fetch(baseUrl, {
+    method: 'POST',
+    body: JSON.stringify({ url: videoUrl, deviceId: makeId(21) }),
+    headers: { 'content-type': 'application/json' },
+  })
+    .then((res) => res.json())
+    .then((res) =>
+      (Object.values((res as SummaryResponse).rollups) ?? [])
+        .map((chunk) => chunk.summary)
+        .join(' '),
     );
-    if (!summaryEl) return;
-
-    return summaryEl.asElement()?.innerText();
-  };
-  const summary = await getSummary();
-  if (!summary) return;
-
-  // Teardown
-  await context.close();
-  await browser.close();
 
   return summary;
+};
+
+const makeId = (length: number) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++)
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+  return result;
 };
 
 // Example usage:
 // const videoUrl = 'https://www.youtube.com/watch?v=ruUlK6zRwS8';
 // const summary = await getVideoSummary(videoUrl);
+// console.log({ summary });
+
+type SummaryResponse = {
+  rollups: Record<number, SummaryChunk>;
+  title: string;
+};
+
+type SummaryChunk = {
+  children: Record<number, string>;
+  summary: string;
+};
