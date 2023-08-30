46808b95af885754c69c772e7db54e623de64090
lekinano <djang0dev@tutanota.com>
2023-08-31 00:34:41 +0200

    refactor: add vxtwitter to isASocialNetworkUrl

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -1,5 +1,5 @@
 const socialNetworksUrlRegex = new RegExp(
-  '^(https?://)?(www.)?(facebook.com|fb.me|twitter.com|instagram.com|linkedin.com|youtube.com|youtu.be|pinterest.com|snapchat.com|tiktok.com)/[a-zA-Z0-9.-/?=&#_]+$',
+  '^(https?://)?(www.)?(facebook.com|fb.me|twitter.com|vxtwitter.com|instagram.com|linkedin.com|youtube.com|youtu.be|pinterest.com|snapchat.com|tiktok.com)/[a-zA-Z0-9.-/?=&#_]+$',
 );
 export const isASocialNetworkUrl = (url: string): boolean => {
   return socialNetworksUrlRegex.test(url);
