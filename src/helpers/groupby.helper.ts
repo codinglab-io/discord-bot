a18f4ef4990f970a996071280672207e5543145d
Manuel <manuel.cartier@kombo.co>
2023-09-03 03:40:06 +0200

    chore: remove unused helper

--- src/helpers/groupby.helper.ts
+++
@@ -1,7 +0,0 @@
-export const groupBy = <T>(arr: T[], fn: (item: T) => string) =>
-  arr.reduce<Record<string, T[]>>((prev, curr) => {
-    const groupKey = fn(curr);
-    const group = prev[groupKey] || [];
-    group.push(curr);
-    return { ...prev, [groupKey]: group };
-  }, {});
