cce59b21a55298beaa33b47582aa51089c5ddf96
LazyRabbit <lmontaigut@gmail.com>
2023-09-04 22:10:34 +0200

    feat: quoi feur (#52)
    Co-authored-by: Peïo Thibault <peio.thibault@gmail.com>
    Co-authored-by: Manu <neolectron@codinglab.io>

---
+++ src/modules/quoiFeur/quoiFeur.module.ts
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+import type { BotModule } from '../../types/bot';
+import {
+  addQuoiFeurToChannel,
+  deleteRoleMutedOnCoubeh,
+  reactOnEndWithQuoi,
+  removeQuoiFeurFromChannel,
+} from './quoiFeur.helpers';
+
+export const quoiFeur: BotModule = {
+  slashCommands: [
+    {
+      schema: new SlashCommandBuilder()
+        .setName('quoi-feur')
+        .setDescription('Manage quoi-feur game in the channel')
+        .addSubcommand((subcommand) =>
+          subcommand.setName('add').setDescription('Add the quoi-feur game to the channel'),
+        )
+        .addSubcommand((subcommand) =>
+          subcommand.setName('remove').setDescription('Remove the quoi-feur game from the channel'),
+        )
+        .toJSON(),
+      handler: {
+        add: addQuoiFeurToChannel,
+        remove: removeQuoiFeurFromChannel,
+      },
+    },
+  ],
+  eventHandlers: {
+    // unmute everyone in every server on bot restart
+    ready: deleteRoleMutedOnCoubeh,
+    messageCreate: reactOnEndWithQuoi,
+  },
+};
