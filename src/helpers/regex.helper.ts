1604ad68171e8ae4c85f60ca9922d92de4024ee4
Luca Montaigut <lmontaigut@gmail.com>
2023-09-04 21:33:09 +0200

    refactor: regex without constructor

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -1,8 +1,8 @@
 const socialNetworksUrlRegex = new RegExp(
   '^(https?://)?(www.)?(facebook.com|fb.me|twitter.com|vxtwitter.com|instagram.com|linkedin.com|youtube.com|youtu.be|pinterest.com|snapchat.com|tiktok.com)/[a-zA-Z0-9.-/?=&#_]+$',
 );
-const punctuationRegex = new RegExp(/[.,!?]/g);
-const emojiRegex = new RegExp(/(\p{Extended_Pictographic}|\p{Emoji_Component})/gu);
+const punctuationRegex = /[.,!?]/g;
+const emojiRegex = /(\p{Extended_Pictographic}|\p{Emoji_Component})/gu;
 
 export const isASocialNetworkUrl = (url: string): boolean => {
   return socialNetworksUrlRegex.test(url);
