42148572dfeeb8c5c1cd241d4f59aee9ca0651a7
Manuel <manuel.cartier@kombo.co>
2023-09-04 16:58:51 +0200

    fix: fire ready event of modules when client is ready

--- src/core/loadModules.ts
+++ src/core/loadModules.ts
@@ -9,8 +9,6 @@ export const loadModules = async (
   client: Client<true>,
   modulesToLoad: Record<string, BotModule>,
 ): Promise<void> => {
-  Object.values(modulesToLoad).forEach((module) => void module.eventHandlers?.ready?.(client));
-
   const botCommands = Object.values(modulesToLoad).flatMap((module) => module.slashCommands ?? []);
   checkUniqueSlashCommandNames(botCommands);
   routeCommands(client, botCommands);
