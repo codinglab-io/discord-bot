aac52ff3b8ca94bc1c8ccdb1073e5c378addc6a1
dilaouid <dilaouid@student.42.fr>
2023-09-13 19:15:14 +0200

    fix(quoi-feur): check if the trigger word is formatted in md (#68)
    Co-authored-by: Peïo Thibault <peio.thibault@gmail.com>

--- src/__tests__/regex.spec.ts
+++ src/__tests__/regex.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { isASocialNetworkUrl, removeEmoji } from '../helpers/regex.helper';
+import { isASocialNetworkUrl, removeEmoji, removeMarkdown } from '../helpers/regex.helper';
 
 describe('Helpers: Regex', () => {
   describe('Rule: isASocialNetworkUrl should regex correctly an url', () => {
@@ -20,6 +20,13 @@ describe('Helpers: Regex', () => {
       const text = '👋 Hello, World!<:SpongebobMock:1136008737669259407>';
       const result = removeEmoji(text);
       expect(result).toBe(' Hello, World!');
+    });
+  });
+  describe('Rule: removeMarkdown should remove all markdown from a string', () => {
+    it('removeMarkdown() should remove all markdown from a string', () => {
+      const text = 'Hello, **World!** This is _me_ I **give** you `reactions` and ~more~ **quoi**';
+      const result = removeMarkdown(text);
+      expect(result).toBe('Hello, World! This is me I give you reactions and more quoi');
     });
   });
 });
