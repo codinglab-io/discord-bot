03df1b4dbec055fa9cf13c081bd630542025fe4d
Manuel <manuel.cartier@kombo.co>
2023-10-15 22:51:49 +0200

    chore: create transport module for formatting in dev

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
+//eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
+  entry: {
+    main: 'src/main.ts',
+    pinoTransportModule: 'src/core/pinoTransportModule.ts',
+  },
   format: ['esm'],
   keepNames: true,
   minify: true,
