fe1cfd32ff4aa7a1deb83273091b0fd8be411c52
Manuel <manuel.cartier@kombo.co>
2023-10-15 20:30:44 +0200

    chore: remove pino-dev

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,32 +1,17 @@
 import createLogger, { type LoggerOptions } from 'pino';
-import type pinodev from 'pino-dev';
 import type { PrettyOptions } from 'pino-pretty';
 
-type PrettyDevOptions = Parameters<typeof pinodev>[0];
-type BotLoggerOptions = LoggerOptions &
-  (
-    | { transport?: { target: 'pino-dev'; options: PrettyDevOptions } }
-    | { transport?: { target: 'pino-pretty'; options: PrettyOptions } }
-  );
+type BotLoggerOptions = LoggerOptions & {
+  transport?: { target: 'pino-pretty'; options: PrettyOptions };
+};
 
 const developmentOptionsOverride: BotLoggerOptions = {
   transport: {
-    target: 'pino-dev',
+    target: 'pino-pretty',
     options: {
       colorize: true,
-      // propertyMap: {
-      //   module: '[module]',
-      // },
     },
   },
-  // transport: {
-  //   target: 'pino-pretty',
-  //   options: {
-  //     customPrettifiers: {
-  //       // module: (name) => `[${name}]`,
-  //     },
-  //   },
-  // },
   level: process.env['LOGLEVEL'] ?? 'debug',
 };
 
